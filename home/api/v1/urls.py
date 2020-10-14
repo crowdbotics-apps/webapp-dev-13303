1c92c164faa6fc640b5021c95ffb7512d5aaec42
Crowdbotics <team@crowdbotics.com>
2020-10-14 12:23:28 +0000

    Update models

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import CustomTextViewSet, HomePageViewSet, TestViewSet
+from .viewsets import CustomTextViewSet, TestViewSet, HomePageViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
